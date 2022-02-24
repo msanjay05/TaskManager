b9b3d29c582252542eeff5e4e78822939019e375
sanjay kumar <sanjayamystery@gmail.com>
2022-02-24 15:43:06 +0530

    m

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api")
+mongoose.connect(process.env.MONGODB_CONNECTION_URL)
 mongoose.set('runValidators', true);
