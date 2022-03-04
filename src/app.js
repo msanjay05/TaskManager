d9ebfe2ed376c546fb5782073648df5bd98bc75a
sanjay kumar <sanjayamystery@gmail.com>
2022-03-04 13:53:43 +0530

    a

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("./db/mongoose");
+const cors = require('cors');
 const userRouter=require("./router/userRouter")
 const taskRouter=require("./router/taskRouter")
 
@@ -7,6 +8,7 @@ const taskRouter=require("./router/taskRouter")
 const app = express();
 
 app.use(express.json());
+app.use(cors)
 app.use(userRouter);
 app.use(taskRouter);
 
